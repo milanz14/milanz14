57e3fe85e4bfcd98d1eb37e3870509d1df25fd46
Milan Zagorac <zagoracm@gmail.com>
2022-05-29 08:12:13 -0400

    setup vue-ts

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <div>Hello, Hyrule</div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+  background: black;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-evenly;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
