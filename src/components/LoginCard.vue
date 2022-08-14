d87dc0f5b093ab874ddc7037efe329b3211f6765
Milan Zagorac <zagoracm@gmail.com>
2022-08-14 09:22:16 -0400

    remove

---
+++ src/components/LoginCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="email" v-model="email" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginCard",
+  data() {
+    return {
+      email: "",
+    };
+  },
+};
+</script>
