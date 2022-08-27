1328d090116895b77de5b389aa7e4707926f9aa2
Milan Zagorac <zagoracm@gmail.com>
2022-08-27 18:08:25 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Header from "./components/Header";
+import AboutCard from "./components/AboutCard";
 import ProjectCard from "./components/ProjectCard";
-import LandingPage from "./components/LandingPage";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div>
-      <Header />
       <div>
+        <Header />
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/" element={<Hero />} />
           <Route exact path="/projects" element={<ProjectCard />} />
+          <Route exact path="/about" element={<AboutCard />} />
         </Routes>
       </div>
     </div>
---
+++ src/components/AboutCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutCard = () => {
+  return (
+    <section id="about">
+      <div className="container flex flex-col items-center px-6 mx-auto mt-10 space-y-0 md:flex-row md:space-y-0">
+        {/*  */}
+      </div>
+    </section>
+  );
+};
+
+export default AboutCard;
