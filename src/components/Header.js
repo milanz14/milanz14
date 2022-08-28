bc8b5894d396e78dc71ef3913618ddfedb8d65de
Milan Zagorac <zagoracm@gmail.com>
2022-08-28 13:31:20 -0400

    hero updates, config updates, memoji hover effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,7 +64,7 @@ const Header = () => {
           id="menu"
           className={
             isOpen
-              ? "absolute flex flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md"
+              ? "absolute flex flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md "
               : "hidden absolute flex flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md"
           }
         >
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,31 @@
 import React from "react";
 import HeroImg from "../assets/hero-img.png";
+import memeoji from "../assets/memeoji.png";
 
 const Hero = () => {
   return (
     <section id="hero">
       <div className="container flex flex-col md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
-        <div className="flex flex-col mb-32 space-y-12 md:w-1/2 md:mb-12">
-          <h1 className="max-width-md bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-4xl font-bold text-center md:text-5xl md:text-left hover:bg-gradient-to-l from-pink-500 to-violet-500">
+        <div className="flex flex-col mb-8 space-y-8 md:w-1/2">
+          <h1 className="max-width-md bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-t from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100 text-5xl font-bold text-center md:text-5xl md:text-left">
             Milan Zagorac <br />
             Web Developer
           </h1>
-          <p className="max-w-sm text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 md:text-left hover:bg-gradient-to-b from-pink-500 to-violet-500">
+          <p className="max-w-sm text-center bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-l from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100 md:text-left">
             Full stack web developer with a passion for JavaScript. My areas of
             expertise are React, Vue, TypeScript and Node.
           </p>
+          <div className="flex flex-col md:hidden transform transition duration-500 hover:scale-105">
+            <img src={memeoji} alt="developer memeoji" />
+          </div>
           <div className="flex justify-center md:justify-start">
-            <a className="p-3 px-6 pt-2 text-white bg-primary rounded-full baseline hover:cursor-pointer hover:bg-gradient-to-t from-pink-500 to-violet-500">
+            <a className="p-3 px-6 pt-2 text-white bg-primary rounded-full baseline hover:cursor-pointer transition-all duration-500 bg-gradient-to-l from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100">
               Contact Me
             </a>
           </div>
         </div>
 
-        <div className="drop-shadow-md md:w-1/2">
+        <div className="hover:drop-shadow-lg md:w-1/2">
           <img src={HeroImg} />
         </div>
       </div>
