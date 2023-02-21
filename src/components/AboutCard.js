3f827eefc2f8fc61ef0fbac667b80d09190b0766
Milan Zagorac <zagoracm@gmail.com>
2023-02-21 17:13:43 -0500

    animations

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,6 +1,24 @@
-import React from "react";
+import { motion } from "framer-motion";
 
 const AboutCard = () => {
+  const skills = [
+    {
+      description: "Front End Skills and Technologies",
+      paragraph:
+        "I am well versed with HTML, CSS, JavaScript, TypeScript, React, Vue, Bulma, Tailwind, Bootstrap, Vuetify, MaterialUI, Chakra andmany more. I believe in writing clean code while minimizing technical debt. I have used Surge and Netlify to deploy front end websites.",
+    },
+    {
+      description: "Backend Skills and Technologies",
+      paragraph:
+        "I have written APIs with Nodejs, Express, Python, Flask using robust technologies such as JWT. I have utilized PostgreSQL (SQL) and MongoDB (NoSQL) for DBs. I am familiar with Firebase as an all-in solution. I have used Heroku, MongoDB Cloud and Firebase to deploy back end projects.",
+    },
+    {
+      description: "Other Technologies and Tools",
+      paragraph:
+        "Terminal. Git. Github. Jasmine. Jest. React-Test-Utils. Redux. Vue-Test-Utils. Vuex. Pinia. Docker. Kubernetes. AWS. VSCode. Atom. Postman. Insomnia. Xcode. Bash scripting. Many more...",
+    },
+  ];
+
   return (
     <section id="about">
       <div className="container flex flex-col px-4 mx-auto mt-10 mt-16 space-y-12 md:space-y-0 md:flex-row">
@@ -17,86 +35,33 @@ const AboutCard = () => {
         </div>
         {/* Numbered List */}
         <div className="flex flex-col space-y-8 md:w-1/2">
-          <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
-            {/* <!-- Heading --> */}
-            <div className="rounded-l-full bg-tertiary md:bg-transparent">
-              <div className="flex items-center space-x-2">
-                <div className="px-4 py-2 text-white rounded-full md:py-1 bg-primary">
-                  <i className="fa-solid fa-check"></i>
+          {skills.map((skill, idx) => (
+            <motion.div
+              initial={{ opacity: 0, translateX: 400 }}
+              animate={{ opacity: 1, translateX: 0 }}
+              transition={{ type: "spring", duration: 0.25 * idx + 1 }}
+            >
+              <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+                <div className="rounded-l-full bg-tertiary md:bg-transparent">
+                  <div className="flex items-center space-x-2">
+                    <div className="px-4 py-2 text-white rounded-full md:py-1 bg-primary">
+                      <i className="fa-solid fa-check"></i>
+                    </div>
+                    <h3 className="text-base font-bold md:mb-4 text-white md:hidden">
+                      {skill.description}
+                    </h3>
+                  </div>
                 </div>
-                <h3 className="text-base font-bold md:mb-4 text-white md:hidden">
-                  Front End Skills and Technologies
-                </h3>
-              </div>
-            </div>
 
-            <div>
-              <h3 className="hidden mb-4 text-lg font-bold md:block">
-                Front End Skills and Technologies
-              </h3>
-              <p className="text-black">
-                I am well versed with HTML, CSS, JavaScript, TypeScript, React,
-                Vue, Bulma, Tailwind, Bootstrap, Vuetify, MaterialUI, Chakra and
-                many more. I believe in writing clean code while minimizing
-                technical debt. I have used Surge and Netlify to deploy front
-                end websites.
-              </p>
-            </div>
-          </div>
-
-          {/* <!-- List Item 2 --> */}
-          <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
-            {/* <!-- Heading --> */}
-            <div className="rounded-l-full bg-tertiary md:bg-transparent">
-              <div className="flex items-center space-x-2">
-                <div className="px-4 py-2 text-white rounded-full md:py-1 bg-primary">
-                  <i className="fa-solid fa-check"></i>
+                <div>
+                  <h3 className="hidden mb-4 text-lg font-bold md:block">
+                    {skill.description}
+                  </h3>
+                  <p className="text-black">{skill.paragraph}</p>
                 </div>
-                <h3 className="text-base font-bold text-white md:mb-4 md:hidden">
-                  Backend Skills and Technologies
-                </h3>
               </div>
-            </div>
-
-            <div>
-              <h3 className="hidden mb-4 text-lg font-bold md:block">
-                Backend Skills and Technologies
-              </h3>
-              <p className="text-black">
-                I have written APIs with Nodejs, Express, Python, Flask using
-                robust technologies such as JWT. I have utilized PostgreSQL
-                (SQL) and MongoDB (NoSQL) for DBs. I am familiar with Firebase
-                as an all-in solution. I have used Heroku, MongoDB Cloud and
-                Firebase to deploy back end projects.
-              </p>
-            </div>
-          </div>
-
-          {/* <!-- List Item 3 --> */}
-          <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
-            {/* <!-- Heading --> */}
-            <div className="rounded-l-full bg-tertiary md:bg-transparent">
-              <div className="flex items-center space-x-2">
-                <div className="px-4 py-2 text-white rounded-full md:py-1 bg-primary">
-                  <i className="fa-solid fa-check"></i>
-                </div>
-                <h3 className="text-base font-bold text-white md:mb-4 md:hidden">
-                  Other Technolgies and Tools
-                </h3>
-              </div>
-            </div>
-
-            <div>
-              <h3 className="hidden mb-4 text-lg font-bold md:block">
-                Other Technolgies and Tools
-              </h3>
-              <p className="text-darkGrayishBlue">
-                Terminal. Git. Github. Jasmine. Jest. React-Test-Utils. Redux.
-                Vue-Test-Utils. Vuex. Pinia. Docker. Kubernetes. AWS. VSCode.
-                Atom. Postman. Insomnia. Xcode. Bash scripting. Many more...
-              </p>
-            </div>
-          </div>
+            </motion.div>
+          ))}
         </div>
       </div>
     </section>
