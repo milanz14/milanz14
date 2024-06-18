1d36692256d1c04991ee55335eccace8ec68665b
Milan Zagorac <zagoracm@gmail.com>
2024-06-18 13:40:34 -0400

    remove framer, fix bug with projects

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import { AnimatePresence, motion, useAnimation } from "framer-motion";
+import { useState } from "react";
 
 import React from "react";
 
@@ -51,57 +50,47 @@ const Carousel = ({ projects }) => {
           className="mt-8 h-full w-full flex justify-center overflow-hidden relative"
           key={projects[currentIndex].name}
         >
-          <AnimatePresence>
-            <motion.div
-              key={currentIndex}
-              initial={{ x: 100 }}
-              animate={{ x: 0 }}
-              exit={{ x: -100 }}
-              transition={{ duration: 0.5 }}
-              className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-800 h-full w-full"
-            >
-              <img
-                className="mt-1 rounded-lg"
-                src={projects[currentIndex].imgSrc}
-                alt="project screenshot"
-              />
-              <h5 className="text-white text-lg font-bold">
-                {projects[currentIndex].subtitle} -{" "}
-                {projects[currentIndex].name}
-              </h5>
-              <p className="text-sm text-white">
-                {projects[currentIndex].description}
-              </p>
-              <div className="flex flex-row items-center">
-                <a
-                  href={projects[currentIndex].githubLink}
-                  target="_blank"
-                  className="text-white px-4"
-                  rel="noopener noreferrer"
+          <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-800 h-full w-full">
+            <img
+              className="mt-1 rounded-lg"
+              src={projects[currentIndex].imgSrc}
+              alt="project screenshot"
+            />
+            <h5 className="text-white text-lg font-bold">
+              {projects[currentIndex].subtitle} - {projects[currentIndex].name}
+            </h5>
+            <p className="text-sm text-white">
+              {projects[currentIndex].description}
+            </p>
+            <div className="flex flex-row items-center">
+              <a
+                href={projects[currentIndex].githubLink}
+                target="_blank"
+                className="text-white px-4"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a
+                href={projects[currentIndex].liveSite}
+                className="text-white px-4"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-solid fa-pager"></i>
+              </a>
+            </div>
+            <div>
+              {projects[currentIndex].tech.map((data, idx) => (
+                <span
+                  key={idx}
+                  className="inline-block bg-tertiary rounded-full px-3 py-2 my-2 text-sm text-white mr-2"
                 >
-                  <i className="fa-brands fa-github"></i>
-                </a>
-                <a
-                  href={projects[currentIndex].liveSite}
-                  className="text-white px-4"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fa-solid fa-pager"></i>
-                </a>
-              </div>
-              <div>
-                {projects[currentIndex].tech.map((data, idx) => (
-                  <span
-                    key={idx}
-                    className="inline-block bg-tertiary rounded-full px-3 py-2 my-2 text-sm text-white mr-2"
-                  >
-                    {data}
-                  </span>
-                ))}
-              </div>
-            </motion.div>
-          </AnimatePresence>
+                  {data}
+                </span>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,57 +6,57 @@ import { useInView } from "react-intersection-observer";
 import { useEffect, useRef } from "react";
 
 const Hero = () => {
-  const { ref, inView } = useInView();
-  const animation = useAnimation();
+  // const { ref, inView } = useInView();
+  // const animation = useAnimation();
 
   // const canvasRef = useRef();
   const imageRef = useRef();
 
-  useEffect(() => {
-    if (inView) {
-      animation.start({
-        translateY: 0,
-        opacity: 1,
-        transition: {
-          duration: 1.75,
-        },
-      });
-    }
-    if (!inView) {
-      animation.start({ translateY: -400, opacity: 0 });
-    }
-  }, [inView, animation]);
+  // useEffect(() => {
+  //   if (inView) {
+  //     animation.start({
+  //       translateY: 0,
+  //       opacity: 1,
+  //       transition: {
+  //         duration: 1.75,
+  //       },
+  //     });
+  //   }
+  //   if (!inView) {
+  //     animation.start({ translateY: -400, opacity: 0 });
+  //   }
+  // }, [inView, animation]);
 
   return (
     <section id="hero" className="h-full">
-      <motion.div animate={animation} ref={ref}>
-        <div className="container h-full flex flex-col md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
-          <div className="flex flex-col items-center mb-8 space-y-8 md:w-1/2">
-            <h1 className="max-width-md bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-t from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100 text-5xl font-bold text-center md:text-5xl ">
-              Milan Zagorac <br />
-              Software <br />
-              Developer
-            </h1>
-            <p className="max-w-sm pt-10 text-left bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-l from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100 lg:text-left">
-              I write code.
-            </p>
-            <div className="flex flex-col md:hidden transform transition duration-500 hover:scale-105">
-              <img src={avatar} alt="developer memeoji" />
-            </div>
-            <div className="flex justify-start md:justify-start">
-              <a
-                href="#contact"
-                className="p-3 px-6 pt-2 text-white bg-primary rounded-full baseline hover:cursor-pointer transition-all duration-500 bg-gradient-to-l from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100"
-              >
-                Contact Me
-              </a>
-            </div>
+      {/* <motion.div animate={animation} ref={ref}> */}
+      <div className="container h-full flex flex-col md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
+        <div className="flex flex-col items-center mb-8 space-y-8 md:w-1/2">
+          <h1 className="max-width-md bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-t from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100 text-5xl font-bold text-center md:text-5xl ">
+            Milan Zagorac <br />
+            Software <br />
+            Developer
+          </h1>
+          <p className="max-w-sm pt-10 text-left bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-l from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100 lg:text-left">
+            I write code.
+          </p>
+          <div className="flex flex-col md:hidden transform transition duration-500 hover:scale-105">
+            <img src={avatar} alt="developer memeoji" />
           </div>
-          <div className="hover:drop-shadow-lg md:w-1/2 transform transition duration-500">
-            <img src={AboutMe} alt="details code snippet" ref={imageRef} />
+          <div className="flex justify-start md:justify-start">
+            <a
+              href="#contact"
+              className="p-3 px-6 pt-2 text-white bg-primary rounded-full baseline hover:cursor-pointer transition-all duration-500 bg-gradient-to-l from-pink-700 via-tertiary to-violet-800 bg-size-200 bg-pos-0 hover:bg-pos-100"
+            >
+              Contact Me
+            </a>
           </div>
         </div>
-      </motion.div>
+        <div className="hover:drop-shadow-lg md:w-1/2 transform transition duration-500">
+          <img src={AboutMe} alt="details code snippet" ref={imageRef} />
+        </div>
+      </div>
+      {/* </motion.div> */}
     </section>
   );
 };
