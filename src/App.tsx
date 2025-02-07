7f65b6b8c107fd59bc04f8680558d353786ab6c7
Milan Zagorac <zagoracm@gmail.com>
2025-02-07 07:48:08 -0500

    update tailwind config: add colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div>
-      <h1 className="text-red-600 underline text-bold text-5xl">Test</h1>
+      <h1 className="text-red-600 underline font-bold font-weight-700 text-5xl font-[Montserrat]">
+        TEST
+      </h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
         tertiary: "#B298DC",
         accent: "#B8D0EB",
         light: "#B9FAF8",
+        dark: "#121212",
+        white: "#fefdfd",
+        studio: "#5f43b2",
+        sunPearl: "#b1aebb",
+        martinique: "#3a3153",
       },
       backgroundSize: {
         "size-200": "200% 200%",
